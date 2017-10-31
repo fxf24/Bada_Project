25c05e37d2d635cee6d8f8f5186a4123bf8ecfb2
Sam Kim <fxf24@naver.com>
2017-10-31 19:25:15 +0900

    start

---
+++ app/src/main/java/com/example/qudqj_000/a2017_10_30/New_gridadapter.java
@@ -0,0 +1,52 @@
+package com.example.qudqj_000.a2017_10_30;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by qudqj_000 on 2017-10-30.
+ */
+
+public class New_gridadapter extends BaseAdapter {
+    Context context;
+    ArrayList<News_Data> data;
+
+    New_gridadapter(Context context, ArrayList<News_Data> data){
+        this.context = context;
+        this.data = data;
+    }
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if(convertView == null){
+            convertView = LayoutInflater.from(context).inflate(R.layout.new_gridview,parent);
+        }
+        ImageView iv1 = (ImageView)convertView.findViewById(R.id.news_image);
+        TextView tv1 = (TextView)convertView.findViewById(R.id.news_title);
+        iv1.setImageResource(Integer.parseInt(data.get(position).getNews_img()));
+        tv1.setText(data.get(position).getNews_title());
+
+        return convertView;
+    }
+}
---
+++ app/src/main/java/com/example/qudqj_000/a2017_10_30/News_Data.java
@@ -0,0 +1,35 @@
+package com.example.qudqj_000.a2017_10_30;
+
+/**
+ * Created by qudqj_000 on 2017-10-30.
+ */
+
+public class News_Data {
+    private String news_img;
+    private String news_title;
+    private String news_html;
+
+    public String getNews_img() {
+        return news_img;
+    }
+
+    public void setNews_img(String news_img) {
+        this.news_img = news_img;
+    }
+
+    public String getNews_title() {
+        return news_title;
+    }
+
+    public void setNews_title(String news_title) {
+        this.news_title = news_title;
+    }
+
+    public String getNews_html() {
+        return news_html;
+    }
+
+    public void setNews_html(String news_html) {
+        this.news_html = news_html;
+    }
+}
